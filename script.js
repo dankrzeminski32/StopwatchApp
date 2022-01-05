2bbca2cf83241c8bc86440f656e8ce9fd9fab900
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-01-05 14:20:39 -0600

    created start of timerView

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ let startStopButton = document.querySelector(".start");
 let resetButton = document.querySelector(".reset");
 let stopwatchButton = document.querySelector("#stopwatch-button");
 let timerButton = document.querySelector("#timer-button");
+let timerView = document.querySelector("#timerView");
 let homeScreen = document.querySelector("#home-screen");
 let stopwatchView = document.querySelector("#stopwatch");
-let backButton = document.querySelector(".back-button");
+let stopwatchBackButton = document.querySelector("#stopwatch-back-button");
+let timerDisplay = document.querySelector(".timer-display");
+let timerBackButton = document.querySelector("#timer-back-button");
 
 var stopWatch = {
   countMilSeconds: 0.0,
+  countSeconds: 0,
+  countMinutes: 0,
+  countHours: 0,
+};
+
+var timer = {
   countSeconds: 0,
   countMinutes: 0,
   countHours: 0,
@@ -21,15 +30,24 @@ var interval = null;
 startStopButton.addEventListener("click", startStop);
 resetButton.addEventListener("click", resetTimer);
 stopwatchButton.addEventListener("click", selectStopwatch);
-backButton.addEventListener("click", goBack);
-
+stopwatchBackButton.addEventListener("click", goBackFromStopwatch);
+timerButton.addEventListener("click", selectTimer);
+timerBackButton.addEventListener("click", goBackFromTimer);
 //Functions
 
+//start our stopwatch display as 00:00.00
 milliseconds = stopWatch.countMilSeconds.toString().padStart(2, "0");
 seconds = stopWatch.countSeconds.toString().padStart(2, "0");
 minutes = stopWatch.countMinutes.toString().padStart(2, "0");
 timeDisplay.innerHTML = minutes + ":" + seconds + "." + milliseconds;
 
+//start our timer display as 00:00.00
+timerSeconds = timer.countSeconds.toString().padStart(2, "0");
+timerMinutes = timer.countMinutes.toString().padStart(2, "0");
+timerHours = timer.countHours.toString().padStart(2, "0");
+timerDisplay.innerHTML = timerHours + ":" + timerMinutes + ":" + timerSeconds;
+
+//This is for switching start -> stop view when someone clicks the button
 function startStop() {
   if (startStopButton.classList.contains("start")) {
     startStopButton.classList.remove("start");
@@ -42,13 +60,11 @@ function startStop() {
   }
 }
 
-//use intervals to add to stopwatch.count
-//when you hit 60, then add to minutes... etc
-
-//May not need to use?
+// run this function every ms to update the clock for the stopwatch
 function startTimer() {
   interval = setInterval(function () {
     if (stopWatch.countHours > 0) {
+      //add hours to the display only if we have to
       stopWatch.countMilSeconds += 1;
       milliseconds = stopWatch.countMilSeconds.toString().padStart(2, "0");
       seconds = stopWatch.countSeconds.toString().padStart(2, "0");
@@ -69,6 +85,7 @@ function startTimer() {
         stopWatch.countMinutes = 0;
       }
     } else {
+      //If we only have minutes do not display hours.
       stopWatch.countMilSeconds += 1;
       milliseconds = stopWatch.countMilSeconds.toString().padStart(2, "0");
       seconds = stopWatch.countSeconds.toString().padStart(2, "0");
@@ -90,11 +107,12 @@ function startTimer() {
   }, 1);
 }
 
+//Stop running the startTimer function every ms
 function stopTimer() {
-  console.log("stopTimer");
   clearInterval(interval);
 }
 
+//reset our stopwatch values to 0, and update our display
 function resetTimer() {
   stopWatch.countHours = 0;
   stopWatch.countMilSeconds = 0;
@@ -109,13 +127,26 @@ function resetTimer() {
   timeDisplay.innerHTML = minutes + ":" + seconds + "." + milliseconds;
 }
 
+//display our stopwatch screen and hide our home screen
 function selectStopwatch() {
   stopwatchView.style.display = "block";
   homeScreen.style.display = "none";
 }
 
-function goBack() {
+//navigate back to our homescreen from our stopwatch view
+function goBackFromStopwatch() {
   stopwatchView.style.display = "none";
   homeScreen.style.display = "flex";
   resetTimer();
+}
+
+//display our timer screen and hide our home homeScreen
+function selectTimer() {
+  timerView.style.display = "block";
+  homeScreen.style.display = "none";
+}
+
+function goBackFromTimer() {
+  timerView.style.display = "none";
+  homeScreen.style.display = "flex";
 }
